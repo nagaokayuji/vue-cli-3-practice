4e1ef67a39da13b49f9c7bf4261bd51593042efd
Yuji Nagaoka <yuji.nagaoka@icloud.com>
2020-09-22 14:37:48 +0900

    up

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import LikeNumber from './components/LikeNumber.vue'
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
+
+Vue.directive("border", {
+  bind() { },
+  inserted() { },
+  update() { },
+  componentUpdated() { },
+  unbind() { },
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
